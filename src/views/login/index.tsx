c28ff47b4fef7a0757a5903cace3af7a29b24139
xiachao <1425894912@qq.com>
2023-05-02 18:24:14 +0800

    feat: test

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -15,7 +15,7 @@ const Login = () => {
 				<div className="login-form">
 					<div className="login-logo">
 						<img className="login-icon" src={logo} alt="logo" />
-						<span className="logo-text">驾校管理系统zsh</span>
+						<span className="logo-text">驾校管理系统</span>
 					</div>
 					<LoginForm />
 				</div>
